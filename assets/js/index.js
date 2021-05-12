33947d605f1730b98f758ee212ef746c6218b64b
markeezy <markkhoo95@gmail.com>
2021-05-11 22:23:10 -0700

    add more MEAT!

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,18 @@
 // Pointers
+var aboutContainer = document.getElementById("about");
+var work_Container = document.getElementById("project");
 var reposContainer = document.getElementById("repos");
+var contaContainer = document.getElementById("contact");
 
 // Global Variables
 
+
+// Display for time since started coding
+let timeSince = document.getElementById("time-since");
+setInterval(function() {
+    let time = dayjs().diff(dayjs(1615262400000),'day',true); // Days since March 8, 2020
+    timeSince.innerText = time.toString().substring(0,10);
+},50);
 
 // Fetch and Display Repos
 let requestURL = 'https://api.github.com/users/markkhoo/repos';
@@ -29,3 +39,43 @@ fetch(requestURL)
     };
 });
 
+// INITIALIZE
+function init () {
+    aboutContainer.setAttribute("style", "display: none");
+    work_Container.setAttribute("style", "display: none");
+    reposContainer.setAttribute("style", "display: none");
+    contaContainer.setAttribute("style", "display: none");
+};
+init();
+
+// Click About Me
+document.getElementById("click_about").addEventListener("click",function(){
+    aboutContainer.setAttribute("style", "display: flex");
+    work_Container.setAttribute("style", "display: none");
+    reposContainer.setAttribute("style", "display: none");
+    contaContainer.setAttribute("style", "display: none");
+});
+
+// Click Work
+document.getElementById("click_work_").addEventListener("click",function(){
+    aboutContainer.setAttribute("style", "display: none");
+    work_Container.setAttribute("style", "display: initial");
+    reposContainer.setAttribute("style", "display: none");
+    contaContainer.setAttribute("style", "display: none");
+});
+
+// Click Github Repos
+document.getElementById("click_repos").addEventListener("click",function(){
+    aboutContainer.setAttribute("style", "display: none");
+    work_Container.setAttribute("style", "display: none");
+    reposContainer.setAttribute("style", "display: initial");
+    contaContainer.setAttribute("style", "display: none");
+});
+
+// Click Contact
+document.getElementById("click_conta").addEventListener("click",function(){
+    aboutContainer.setAttribute("style", "display: none");
+    work_Container.setAttribute("style", "display: none");
+    reposContainer.setAttribute("style", "display: none");
+    contaContainer.setAttribute("style", "display: flex");
+});
